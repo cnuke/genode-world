4d2e542c1f120b225a02fdba309223c519db29cb
Josef Söntgen <josef.soentgen@genode-labs.com>
2019-02-05 15:05:04 +0100

    Add port of LittlevGL v5.3

---
+++ lib/import/import-lvgl.mk
@@ -0,0 +1,4 @@
+LIBLVGL_PORT_DIR := $(call select_from_ports,littlevgl)
+INC_DIR += $(LIBLVGL_PORT_DIR)/include/littlevgl
+INC_DIR += $(LIBLVGL_PORT_DIR)/src/lib/littlevgl
+INC_DIR += $(REP_DIR)/src/lib/lvgl
