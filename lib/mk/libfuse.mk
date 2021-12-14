19f5e9faf04ac28f6ad66d85c65f2b7af0482e7d
Josef Söntgen <josef.soentgen@genode-labs.com>
2021-12-14 18:05:53 +0100

    Revert -Dxxx=fuselibc_xxx

--- lib/mk/libfuse.mk
+++ lib/mk/libfuse.mk
@@ -9,13 +9,13 @@ INC_DIR += $(REP_DIR)/include/fuse \
 
 # We are linked against VFS plug-ins, which cannot be compiled with "LIBS = libc".
 # So alias the needed LibC symbols to our simplified implementation instead:
-CC_OPT += -Dopen=fuselibc_open -Dclose=fuselibc_close \
-	-Dpread=fuselibc_pread -Dpwrite=fuselibc_pwrite \
-	-Dstat=fuselibc_stat -Dseek=fuselibc_seek -Dsync=fuselibc_sync \
-	-Dfstat=fuselibc_fstat -Dlseek=fuselibc_lseek -Dfsync=fuselibc_fsync \
-	-Dioctl=fuselibc_ioctl -Dfcntl=fuselibc_fcntl \
-	-Dmalloc=fuselibc_malloc -Dcalloc=fuselibc_calloc \
-	-Dfprintf=fuselibc_fprintf
+# CC_OPT += -Dopen=fuselibc_open -Dclose=fuselibc_close \
+# 	-Dpread=fuselibc_pread -Dpwrite=fuselibc_pwrite \
+# 	-Dstat=fuselibc_stat -Dseek=fuselibc_seek -Dsync=fuselibc_sync \
+# 	-Dfstat=fuselibc_fstat -Dlseek=fuselibc_lseek -Dfsync=fuselibc_fsync \
+# 	-Dioctl=fuselibc_ioctl -Dfcntl=fuselibc_fcntl \
+# 	-Dmalloc=fuselibc_malloc -Dcalloc=fuselibc_calloc \
+# 	-Dfprintf=fuselibc_fprintf
 	#-Dread=fuselibc_read -Dwrite=fuselibc_write : messes with Readonly_File.read()
 	#-Dfree=fuselibc_free  -Drealloc=fuselibc_realloc : messes with avlallocator.free()
 
