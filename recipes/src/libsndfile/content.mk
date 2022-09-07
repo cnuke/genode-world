181f35ad646d272da3c04b87438d86da88c0ac84
Josef Söntgen <josef.soentgen@genode-labs.com>
2022-09-07 11:27:45 +0200

    fixup 'Remove pseudo targets for building shared libs' (libsndfile)
    Since mirror from rep is called first 'src/lib/libsndfile' already
    exists and copying from the port dir is not executed.

--- recipes/src/libsndfile/content.mk
+++ recipes/src/libsndfile/content.mk
@@ -4,16 +4,16 @@ MIRROR_FROM_REP_DIR = \
 	lib/mk/alac.mk \
 	lib/mk/libsndfile.mk \
 	lib/import/import-libsndfile.mk \
-	src/lib/libsndfile/config.h \
+	src/lib/libsndfile/config.h
 
-content: $(MIRROR_FROM_REP_DIR) src/lib/libsndfile LICENSE
+content: $(MIRROR_FROM_REP_DIR) src/lib/libsndfile/src LICENSE
 
 $(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
 
 PORT_DIR := $(call port_dir,$(REP_DIR)/ports/libsndfile)
 
-src/lib/libsndfile:
+src/lib/libsndfile/src:
 	mkdir -p src/lib/libsndfile
 	cp -r $(PORT_DIR)/src/lib/libsndfile/* src/lib/libsndfile
 
