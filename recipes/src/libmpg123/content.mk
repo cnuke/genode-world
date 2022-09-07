d5bfc3dcbe267e6aad10e282896ab29513603f14
Norman Feske <norman.feske@genode-labs.com>
2022-09-07 11:31:44 +0200

    fixup "Remove pseudo targets for building shared libs" (libmpg123)

--- recipes/src/libmpg123/content.mk
+++ recipes/src/libmpg123/content.mk
@@ -1,5 +1,4 @@
 MIRROR_FROM_REP_DIR = \
-	src/lib/mpg123/config.h \
 	lib/import/import-libmpg123.mk \
 	lib/mk/libmpg123.inc \
 	lib/mk/spec/x86_64/libmpg123.mk \
@@ -15,8 +14,9 @@ PORT_DIR := $(call port_dir,$(REP_DIR)/ports/mpg123)
 content: src/lib/mpg123 LICENSE
 
 src/lib/mpg123:
-	mkdir -p src/lib/mpg123
-	cp -r $(PORT_DIR)/src/lib/mpg123/* src/lib/mpg123
+	mkdir -p $@
+	cp -r $(PORT_DIR)/src/lib/mpg123/*       $@
+	cp    $(REP_DIR)/src/lib/mpg123/config.h $@
 
 LICENSE:
 	cp $(PORT_DIR)/src/lib/mpg123/COPYING $@
