ae323f107f4f70cecb9b7ea3ccb3940b0e228a7e
Josef Söntgen <josef.soentgen@genode-labs.com>
2023-10-24 13:25:07 +0200

    Revert "DBG linphone-simple: allow for building directly"
    This reverts commit 846d5175d37b1d7d2e7105cb540cf37470c3be8d.

--- src/app/linphone-simple/target.mk
+++ src/app/linphone-simple/target.mk
@@ -4,9 +4,6 @@ LP_QML_TAR         := $(BUILD_BASE_DIR)/bin/linphone-simple_qml.tar
 LP_QML_ARCHIVE_DIR := linphone-simple
 
 $(TARGET): $(LP_QML_TAR)
-	$(VERBOSE)touch $(BUILD_BASE_DIR)/bin/$(TARGET)
-	$(VERBOSE)touch $(BUILD_BASE_DIR)/bin/linphone.pro.qmake_target
-	$(VERBOSE)touch $(BUILD_BASE_DIR)/bin/servicecontrol.pro.qmake_target
 
 remove_stale:
 	$(VERBOSE)rm $(LP_QML_TAR) || true
