2a878767551a0fd0256790aba29f2f90f7a9d259
Josef Söntgen <josef.soentgen@genode-labs.com>
2023-11-07 16:36:29 +0100

    linphone-simple: initial import
    This commit imports the code of 'SIP Client for Ubuntu Touch' from [1].
    The 'AboutPage.qml' was adapted by hand from the 'about.qml.in'
    template.
    
    [1] https://gitlab.com/ubports-linphone/linphone-simple/-/archive/4c03a020/linphone-simple-4c03a020.tar.gz

---
+++ src/app/linphone-simple/plugins/Linphone/linphone.cpp
@@ -0,0 +1,173 @@
+#include <QDebug>
+#include <QSettings>
+
+#include "linphone.h"
+
+Linphone::Linphone() {
+    connect(&m_linphoneProcess, SIGNAL(finished(int, QProcess::ExitStatus)), this, SLOT(linphoneProcessFinished(int, QProcess::ExitStatus)));
+    connect(&m_linphoneProcess, SIGNAL(readyReadStandardOutput()), this, SIGNAL(readStatus()));
+
+    m_env = QProcessEnvironment::systemEnvironment();
+    m_env.insert("LD_LIBRARY_PATH", m_libPath);
+    m_env.insert("TMPDIR", m_tmpdir);
+}
+
+void Linphone::call(QString address) {
+    QStringList args;
+    args << "dial" << address;
+    m_linphoneProcess.setProcessEnvironment(m_env);
+    m_linphoneProcess.start(m_linphonecsh, args);
+
+    qDebug() << "LINPHONECSH: calling" << address;
+}
+
+void Linphone::terminate() {
+    QStringList args;
+    args << "generic" << "terminate";
+    m_linphoneProcess.setProcessEnvironment(m_env);
+    m_linphoneProcess.start(m_linphonecsh, args);
+
+    qDebug() << "LINPHONECSH: terminating call" << &m_env;
+}
+
+void Linphone::answer() {
+	//TODO Might it be needed to specify wich call?
+    QStringList args;
+    args << "generic" << "answer";
+    m_linphoneProcess.setProcessEnvironment(m_env);
+    m_linphoneProcess.start(m_linphonecsh, args);
+
+    qDebug() << "LINPHONECSH: answering call";
+}
+
+void Linphone::mute() {
+    QStringList args;
+    args << "mute";
+    m_linphoneProcess.setProcessEnvironment(m_env);
+    m_linphoneProcess.start(m_linphonecsh, args);
+
+    qDebug() << "LINPHONECSH: muting call";
+}
+
+void Linphone::unmute() {
+    QStringList args;
+    args << "unmute";
+    m_linphoneProcess.setProcessEnvironment(m_env);
+    m_linphoneProcess.start(m_linphonecsh, args);
+
+    qDebug() << "LINPHONECSH: unmutting call";
+}
+
+void Linphone::registerSIP(QString user, QString domain, QString password) {
+    QStringList args;
+    QString userUser;
+    QString domainHost;
+    QString passwordPassword;
+    //address = "sip:" + user + "@" + domain;
+    userUser = " --username " + user;
+    domainHost = " --host " + domain;
+    passwordPassword = " --password " + password;
+    args << "register" << "--username" << user << "--host" << domain << "--password" << password;
+    qDebug() << "register" << "--username" << user << "--host" << domain;
+    m_linphoneProcess.setProcessEnvironment(m_env);
+    m_linphoneProcess.start(m_linphonecsh, args);
+
+    qDebug() << "LINPHONECSH: registering " << user;
+
+    status("register");
+}
+
+void Linphone::status(QString whatToCheck) {
+    QStringList args;
+    args << "status" << whatToCheck;
+
+    m_linphoneProcess.setProcessEnvironment(m_env);
+    m_linphoneProcess.start(m_linphonecsh, args);
+
+    qDebug() << "LINPHONECSH: status on " << whatToCheck;
+
+}
+
+QString Linphone::readStatusOutput() {
+
+	QByteArray bytes = m_linphoneProcess.readAllStandardOutput();
+    QString output = QString::fromLocal8Bit(bytes);
+
+    qDebug() << "LINPHONECSH: output " << output;
+    return output;
+}
+
+void Linphone::command(QStringList userCommand) {
+    QStringList args;
+    args << userCommand;
+
+    m_linphoneProcess.setProcessEnvironment(m_env);
+    m_linphoneProcess.start(m_linphonecsh, args);
+
+    qDebug() << "LINPHONECSH: command " << userCommand;
+}
+
+void Linphone::enableSpeaker() {
+    QProcess pacmdProcess;
+    QStringList args;
+    args << "set-sink-port" << "0" << "output-speaker";
+
+    pacmdProcess.start("pacmd", args);
+    pacmdProcess.waitForFinished();  // This shouldn't take too long
+}
+
+void Linphone::disableSpeaker() {
+    QProcess pacmdProcess;
+    QStringList args;
+    args << "set-sink-port" << "0" << "output-earpiece";
+
+    pacmdProcess.start("pacmd", args);
+    pacmdProcess.waitForFinished();  // This shouldn't take too long
+}
+
+void Linphone::displayOn() {
+    QProcess powerdCliProcess;
+    QStringList args;
+    args << "display" << "on";
+
+    powerdCliProcess.start("powerd-cli", args);
+    //Let's wait 3/4 of a second to be «finished» otherwise screen doesn't turn on (as QProcess is killed before)
+    powerdCliProcess.waitForFinished(750); 
+}
+
+void Linphone::linphoneProcessFinished(int exitCode, QProcess::ExitStatus exitStatus) {
+    //Extra paranoid logging
+    qDebug() << "LINPHONECSH: stdout" << m_linphoneProcess.readAllStandardOutput();
+    qDebug() << "LINPHONECSH: stderr" << m_linphoneProcess.readAllStandardError();
+    qDebug() << "LINPHONECSH: exit code" << m_linphoneProcess.exitCode();
+
+    //More info http://doc.qt.io/qt-5/qprocess.html#ProcessError-enum
+    if (m_linphoneProcess.error() == QProcess::FailedToStart) {
+        qDebug() << "LINPHONECSH: error: QProcess::FailedToStart";
+    }
+    else if (m_linphoneProcess.error() == QProcess::Crashed) {
+        qDebug() << "LINPHONECSH: error: QProcess::Crashed";
+    }
+    else if (m_linphoneProcess.error() == QProcess::Timedout) {
+        qDebug() << "LINPHONECSH: error: QProcess::Timedout";
+    }
+    else if (m_linphoneProcess.error() == QProcess::WriteError) {
+        qDebug() << "LINPHONECSH: error: QProcess::WriteError";
+    }
+    else if (m_linphoneProcess.error() == QProcess::ReadError) {
+        qDebug() << "LINPHONECSH: error: QProcess::ReadError";
+    }
+    else {
+        if (m_linphoneProcess.exitCode() == 0) {
+            qDebug() << "LINPHONECSH: no errors running the process";
+        }
+        else {
+            qDebug() << "LINPHONECSH: There was an unknown error";
+        }
+    }
+}
+
+void Linphone::setConfig(QString key, QString value) {
+    QSettings settings(m_configFile, QSettings::IniFormat);
+    settings.setValue(key, value);
+}
---
+++ src/app/linphone-simple/plugins/Linphone/linphone.h
@@ -0,0 +1,45 @@
+#ifndef LINPHONE_H
+#define LINPHONE_H
+
+#include <QObject>
+#include <QProcess>
+
+class Linphone: public QObject {
+    Q_OBJECT
+
+public:
+    Linphone();
+    ~Linphone() = default;
+
+    Q_INVOKABLE void call(QString address);
+    Q_INVOKABLE void terminate();
+    Q_INVOKABLE void answer();
+    Q_INVOKABLE void mute();
+    Q_INVOKABLE void unmute();
+    Q_INVOKABLE void registerSIP(QString user, QString domain, QString password);
+    Q_INVOKABLE void status(QString whatToCheck);
+    Q_INVOKABLE void command(QStringList userCommand);
+    Q_INVOKABLE void setConfig(QString key, QString value);
+    Q_INVOKABLE void enableSpeaker();
+    Q_INVOKABLE void disableSpeaker();
+    Q_INVOKABLE void displayOn();
+
+Q_SIGNALS:
+    void readStatus();
+
+public Q_SLOTS:
+    void linphoneProcessFinished(int exitCode, QProcess::ExitStatus exitStatus);
+    QString readStatusOutput();
+
+private:
+    //TODO don't hardcode these
+    QString m_libPath = "/opt/click.ubuntu.com/linphone.cibersheep/current/linphone/lib/arm-linux-gnueabihf";
+    QString m_linphonecsh = "/opt/click.ubuntu.com/linphone.cibersheep/current/linphone/bin/linphonecsh";
+    QString m_tmpdir = "/home/phablet/.cache/linphone-tmp/";
+    QString m_configFile = "/home/phablet/.linphonerc";
+
+    QProcessEnvironment m_env;
+    QProcess m_linphoneProcess;
+};
+
+#endif
