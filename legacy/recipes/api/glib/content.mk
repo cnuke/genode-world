857ed202a841e074e3f806d3649f617ee6d01a5b
Josef Söntgen <josef.soentgen@genode-labs.com>
2025-02-11 17:23:58 +0100

    api/glib: add pkg-config files

--- legacy/recipes/api/glib/content.mk
+++ legacy/recipes/api/glib/content.mk
@@ -1,5 +1,5 @@
 MIRROR_FROM_REP_DIR := lib/symbols/glib
-content: include $(MIRROR_FROM_REP_DIR) LICENSE
+content: include $(MIRROR_FROM_REP_DIR) LICENSE glib-2.0.pc
 
 PORT_DIR := $(call port_dir,$(REP_DIR)/ports/glib)
 
@@ -13,3 +13,14 @@ $(MIRROR_FROM_REP_DIR):
 
 LICENSE:
 	cp $(PORT_DIR)/src/lib/glib/COPYING $@
+
+VERSION := $(shell sed -n 's/VERSION.*:=[ ]*\(.*\)/\1/p' $(REP_DIR)/ports/glib.port)
+
+glib-2.0.pc:
+	echo "glib_genmarshal=glib-genmarshal" > $@
+	echo "gobject_query=gobject-query" >> $@
+	echo "glib_mkenums=glib-mkenums" >> $@
+	echo "Name: Glib" >> $@
+	echo "Description: C Utility Library" >> $@
+	echo "Version: $(VERSION)" >> $@
+	echo "Libs: -l:glib.lib.so" >> $@
